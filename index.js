65e56f9df454d5c93bb30dbc06fe6f8f9bac79c2
TerryEbdon <47636739+TerryEbdon@users.noreply.github.com>
2019-02-16 10:26:32 +0000

    Re-commit of the 2017 site with all 15 instances of the "White Left Pointing Backhand Index" character replaced with `&#x1F448;`, to fix a file encoding issue.

---
+++ index.js
@@ -0,0 +1,86 @@
+var index =
+[
+    [ "Parsing SimpleMind XML - Part One", "wip20170711.html", [
+      [ "The Problems", "wip20170711.html#autotoc_md139", null ],
+      [ "The SMMX files", "wip20170711.html#autotoc_md140", null ],
+      [ "Parsing the XML", "wip20170711.html#autotoc_md141", null ],
+      [ "Related web pages.", "wip20170711.html#autotoc_md142", null ]
+    ] ],
+    [ "Splitting the windows path", "wip20170710.html", [
+      [ "Splitting the windows path", "wip20170710.html#autotoc_md136", null ],
+      [ "Splitting with Groovy", "wip20170710.html#autotoc_md137", null ],
+      [ "Useful output", "wip20170710.html#autotoc_md138", null ]
+    ] ],
+    [ "Verifying Checksums with Ant & Groovy", "wip20170709.html", [
+      [ "The test data", "wip20170709.html#autotoc_md129", null ],
+      [ "An Ant example", "wip20170709.html#autotoc_md130", null ],
+      [ "Collecting the files", "wip20170709.html#autotoc_md131", null ],
+      [ "Searching for signatures", "wip20170709.html#autotoc_md132", null ],
+      [ "Immutable properties", "wip20170709.html#autotoc_md133", null ],
+      [ "Calling checksum from Groovy", "wip20170709.html#autotoc_md134", null ],
+      [ "Related web pages", "wip20170709.html#autotoc_md135", null ]
+    ] ],
+    [ "Using Zotero with Doxygen", "wip20170708.html", [
+      [ "Export from Zotero", "wip20170708.html#autotoc_md124", null ],
+      [ "Tell Doxygen about the new BibTeX file", "wip20170708.html#autotoc_md125", null ],
+      [ "Cite the exported article in markdown", "wip20170708.html#autotoc_md126", null ],
+      [ "Create output as HTML or latex and PDF", "wip20170708.html#autotoc_md127", null ],
+      [ "Example bibliography", "wip20170708.html#autotoc_md128", null ]
+    ] ],
+    [ "Doxygen Citations", "wip20170707.html", [
+      [ "Citations with Doxygen", "wip20170707.html#autotoc_md112", null ],
+      [ "Download and install the software:", "wip20170707.html#autotoc_md113", null ],
+      [ "Configure Doxygen", "wip20170707.html#autotoc_md114", null ],
+      [ "Create a Doxygen configuration file", "wip20170707.html#autotoc_md115", null ],
+      [ "Create a markdown file", "wip20170707.html#autotoc_md116", null ],
+      [ "Create a BibTeX file", "wip20170707.html#autotoc_md117", null ],
+      [ "Enable Latex and / or HTML", "wip20170707.html#autotoc_md118", null ],
+      [ "Running Doxygen", "wip20170707.html#autotoc_md119", null ],
+      [ "HTML Output", "wip20170707.html#autotoc_md120", null ],
+      [ "Latex output", "wip20170707.html#autotoc_md121", null ],
+      [ "Interesting web pages", "wip20170707.html#autotoc_md122", null ],
+      [ "Interesting Tweets", "wip20170707.html#autotoc_md123", null ]
+    ] ],
+    [ "New code and new bio", "wip20170706.html", [
+      [ "Bio update, photography and kit", "wip20170706.html#autotoc_md108", null ],
+      [ "WebDoxy superscript enhancement", "wip20170706.html#autotoc_md109", [
+        [ "Code Structure", "wip20170706.html#autotoc_md110", null ]
+      ] ],
+      [ "Interesting web pages", "wip20170706.html#autotoc_md111", null ]
+    ] ],
+    [ "Problems, old & new", "wip20170705.html", [
+      [ "Doxygen superscript problem solved", "wip20170705.html#autotoc_md104", null ],
+      [ "Doygen TREEVIEW resists re-sizing", "wip20170705.html#autotoc_md105", null ],
+      [ "Related Pages excludes days in current month", "wip20170705.html#autotoc_md106", null ],
+      [ "Interesting web pages", "wip20170705.html#autotoc_md107", null ]
+    ] ],
+    [ "Fixing Doxygen", "wip20170704.html", [
+      [ "Speech marks fail in a table", "wip20170704.html#autotoc_md98", null ],
+      [ "Doxygen Sites on Small Screens", "wip20170704.html#autotoc_md99", null ],
+      [ "Interesting web pages", "wip20170704.html#autotoc_md100", [
+        [ "MarkDown and static site generation", "wip20170704.html#autotoc_md101", null ],
+        [ "BitBucket", "wip20170704.html#autotoc_md102", null ],
+        [ "Mercurial", "wip20170704.html#autotoc_md103", null ]
+      ] ]
+    ] ],
+    [ "HSPs, skinning & data generation", "wip20170703.html", [
+      [ "Web Hosts and Platforms", "wip20170703.html#autotoc_md95", null ],
+      [ "Doxygen theme", "wip20170703.html#autotoc_md96", null ],
+      [ "Creating Test Data for Doxygen", "wip20170703.html#autotoc_md97", null ]
+    ] ],
+    [ "Configuration saves the day", "wip20170702.html", [
+      [ "The unwanted '0' prefix", "wip20170702.html#autotoc_md93", null ],
+      [ "Interesting web pages", "wip20170702.html#autotoc_md94", null ]
+    ] ],
+    [ "BitBucket & Bug Fixes", "wip20170701.html", [
+      [ "Committing code closes the bug. Nice!", "wip20170701.html#autotoc_md85", null ],
+      [ "Daily page title has '0' prefix on date", "wip20170701.html#autotoc_md86", null ],
+      [ "Interesting web pages", "wip20170701.html#autotoc_md87", [
+        [ "CSS", "wip20170701.html#autotoc_md88", null ],
+        [ "Software Development Tools", "wip20170701.html#autotoc_md89", null ],
+        [ "BitBucket and Mercurial", "wip20170701.html#autotoc_md90", null ],
+        [ "AsciiDoc and JavaDoc", "wip20170701.html#autotoc_md91", null ],
+        [ "Groovy", "wip20170701.html#autotoc_md92", null ]
+      ] ]
+    ] ]
+];

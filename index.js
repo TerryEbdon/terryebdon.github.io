6ce74a0261a8bf6651cb9ed7761586bf2fbde639
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2020-06-02 22:34:06 +0100

    2020-06-02 22:28

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ var index =
     [ "June, 2020", "index.html#h202006", null ],
     [ "01-Jun-2020 Weekly page classes", "wip20200601.html", [
       [ "Class collaboration", "wip20200601.html#h20200601", null ]
+    ] ],
+    [ "02-Jun-2020 WebDoxy configuration", "wip20200602.html", [
+      [ "Config file", "wip20200602.html#h20200602", null ]
     ] ]
 ];

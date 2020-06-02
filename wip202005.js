5993ec79f542992648e3dcbff39749daa4f8cffd
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2020-06-02 12:08:00 +0100

    2020-06-01 16:02

---
+++ wip202005.js
@@ -0,0 +1,42 @@
+var wip202005 =
+[
+    [ "May, 2020", "wip202005.html#h202005", null ],
+    [ "06-May-2020 Sad news", "wip20200506.html", [
+      [ "Death of a music legend", "wip20200506.html#autotoc_md257", null ]
+    ] ],
+    [ "11-May-2020 Radio Power", "wip20200511.html", [
+      [ "Radio Power", "wip20200511.html#autotoc_md258", null ]
+    ] ],
+    [ "13-May-2020 Atom fail", "wip20200513.html", [
+      [ "Atom spell checking", "wip20200513.html#autotoc_md259", null ],
+      [ "Changing editors", "wip20200513.html#autotoc_md260", null ]
+    ] ],
+    [ "14-May-2020 VS Code - Commit Signing & Issues", "wip20200514.html", [
+      [ "VS Code", "wip20200514.html#autotoc_md261", [
+        [ "Commit signing", "wip20200514.html#autotoc_md262", null ],
+        [ "GitHub issue integration", "wip20200514.html#autotoc_md263", [
+          [ "Create issues from comments", "wip20200514.html#autotoc_md264", null ],
+          [ "Issues bar", "wip20200514.html#autotoc_md265", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "20-May-2020 GitHub Pages", "wip20200520.html", [
+      [ "Slow GitHub Pages deployment", "wip20200520.html#h20200520", [
+        [ "Bad UTF-8 breaks deployment", "wip20200520.html#autotoc_md268", null ],
+        [ "Missing pages", "wip20200520.html#autotoc_md269", null ]
+      ] ]
+    ] ],
+    [ "24-May-2020 Card performance", "wip20200524.html", [
+      [ "Card performance", "wip20200524.html#h20200524", null ]
+    ] ],
+    [ "26-May-2020 Creating weekly pages", "wip20200526.html", [
+      [ "Weekly changes", "wip20200526.html#h20200526", null ]
+    ] ],
+    [ "30-May-2020 Smelly design", "wip20200530.html", [
+      [ "WebDoxy design changes", "wip20200530.html#h20200530", null ]
+    ] ],
+    [ "31-May-2020 WebDoxy Weekly pages", "wip20200531.html", [
+      [ "Page Hierarchy", "wip20200531.html#h20200531", null ],
+      [ "Page creation", "wip20200531.html#autotoc_md270", null ]
+    ] ]
+];

bf3b0aabef5814523b2039b77d25df22ae501c5b
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2020-08-02 16:30:08 +0100

    2020-08-02 16:28

--- wip202007.js
+++ wip202007.js
@@ -1,36 +1,6 @@
-/*
- @licstart  The following is the entire license notice for the JavaScript code in this file.
-
- The MIT License (MIT)
-
- Copyright (C) 1997-2020 by Dimitri van Heesch
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of this software
- and associated documentation files (the "Software"), to deal in the Software without restriction,
- including without limitation the rights to use, copy, modify, merge, publish, distribute,
- sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all copies or
- substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
- BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
- @licend  The above is the entire license notice for the JavaScript code in this file
-*/
-var NAVTREE =
+var wip202007 =
 [
-  [ "Developer's Diary", "index.html", [
-    [ "02-Aug-2020 awk regex groups", "index.html", [
-      [ "Retrieving regex groups in awk", "index.html#h20200802", [
-        [ "Disconnect version 1", "index.html#autotoc_md310", null ],
-        [ "Disconnect version 2", "index.html#autotoc_md311", null ]
-      ] ]
-    ] ],
+    [ "July, 2020", "wip202007.html#h202007", null ],
     [ "09-Jul-2020 Gradle broken", "wip20200709.html", [
       [ "Mixed compilation", "wip20200709.html#h20200709", null ],
       [ "New Groovy projects fail", "wip20200709.html#autotoc_md288", null ],
@@ -86,18 +56,5 @@ var NAVTREE =
         [ "Static analysis", "wip20200720.html#autotoc_md308", null ],
         [ "Logging", "wip20200720.html#autotoc_md309", null ]
       ] ]
-    ] ],
-    [ "Archives", "archive.html", "archive" ],
-    [ "Photography", "photography.html", "photography" ],
-    [ "About Me", "AboutMe.html", null ]
-  ] ]
+    ] ]
 ];
-
-var NAVTREEINDEX =
-[
-"AboutMe.html",
-"wip20190312.html#autotoc_md192"
-];
-
-var SYNCONMSG = 'click to disable panel synchronisation';
-var SYNCOFFMSG = 'click to enable panel synchronisation';

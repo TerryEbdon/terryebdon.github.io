7e000971171239e7d8852c705b0bad970cef1587
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2020-08-09 13:45:38 +0100

    2020-08-09 13:31

--- navtreedata.js
+++ navtreedata.js
@@ -25,64 +25,13 @@
 var NAVTREE =
 [
   [ "Developer's Diary", "index.html", [
-    [ "07-Aug-2020 Product photos", "index.html", [
-      [ "Stability", "index.html#h20200807", null ],
-      [ "Canon 5D Mark III", "index.html#autotoc_md324", null ]
-    ] ],
-    [ "09-Jul-2020 Gradle broken", "wip20200709.html", [
-      [ "Mixed compilation", "wip20200709.html#h20200709", null ],
-      [ "New Groovy projects fail", "wip20200709.html#autotoc_md288", null ],
-      [ "Old vs New", "wip20200709.html#autotoc_md289", null ]
-    ] ],
-    [ "10-Jul-2020 Gradle fixed", "wip20200710.html", [
-      [ "Clean Gradle", "wip20200710.html#h20200710", null ],
-      [ "Up-to-date", "wip20200710.html#autotoc_md290", [
-        [ "Missing classes", "wip20200710.html#autotoc_md291", null ],
-        [ "Failing configuration", "wip20200710.html#autotoc_md292", null ]
-      ] ],
-      [ "Problem solved", "wip20200710.html#autotoc_md293", [
-        [ "Working configuration", "wip20200710.html#autotoc_md294", null ]
-      ] ]
-    ] ],
-    [ "11-Jul-2020 Gradle rolled forward", "wip20200711.html", [
-      [ "Gradle version", "wip20200711.html#h20200711", null ],
-      [ "Change of plans", "wip20200711.html#autotoc_md295", null ]
-    ] ],
-    [ "12-Jul-2020 Groovy, Gradle & Eclipse", "wip20200712.html", [
-      [ "Eclipse issues", "wip20200712.html#h20200712", null ],
-      [ "Add GUI classes to Groovy", "wip20200712.html#autotoc_md296", null ],
-      [ "Version conflicts", "wip20200712.html#autotoc_md297", null ]
-    ] ],
-    [ "15-Jul-2020 Data in the UI", "wip20200715.html", [
-      [ "Data bound controls", "wip20200715.html#h20200715", null ]
-    ] ],
-    [ "16-Jul-2020 A rough plan", "wip20200716.html", [
-      [ "Distractions", "wip20200716.html#h20200716", null ],
-      [ "Legacy", "wip20200716.html#autotoc_md298", null ],
-      [ "The plan", "wip20200716.html#autotoc_md299", [
-        [ "Reporting", "wip20200716.html#autotoc_md300", null ]
-      ] ]
-    ] ],
-    [ "17-Jul-2020 The big picture", "wip20200717.html", [
-      [ "Photo Shoot", "wip20200717.html#h20200717", null ],
-      [ "Console Input", "wip20200717.html#autotoc_md301", null ]
-    ] ],
-    [ "18-Jul-2020 NPP Progress", "wip20200718.html", [
-      [ "GUI Created", "wip20200718.html#h20200718", null ],
-      [ "Form Layout", "wip20200718.html#autotoc_md302", null ],
-      [ "Mixed compilation", "wip20200718.html#autotoc_md303", null ],
-      [ "Eclipse, a mixed bag", "wip20200718.html#autotoc_md304", null ],
-      [ "Progress", "wip20200718.html#autotoc_md305", null ]
-    ] ],
-    [ "19-Jul-2020 Unit testing", "wip20200719.html", [
-      [ "Web testing", "wip20200719.html#h20200719", null ]
-    ] ],
-    [ "20-Jul-2020 Back to basics", "wip20200720.html", [
-      [ "Infrastructure", "wip20200720.html#h20200720", [
-        [ "Unit tests", "wip20200720.html#autotoc_md306", null ],
-        [ "Test coverage", "wip20200720.html#autotoc_md307", null ],
-        [ "Static analysis", "wip20200720.html#autotoc_md308", null ],
-        [ "Logging", "wip20200720.html#autotoc_md309", null ]
+    [ "08-Jul-2020 Monitors, radio & photos", "index.html", [
+      [ "08-Jul-2020 Monitors, radio & photos", "index.html#wip20200808", [
+        [ "Tech photos", "index.html#h20200808", null ],
+        [ "Monitor splitters", "index.html#autotoc_md325", null ],
+        [ "Remote operation", "index.html#autotoc_md326", [
+          [ "Cable fail", "index.html#autotoc_md327", null ]
+        ] ]
       ] ]
     ] ],
     [ "02-Aug-2020 awk regex groups", "wip20200802.html", [
@@ -116,6 +65,10 @@ var NAVTREE =
         [ "The distro", "wip20200806.html#autotoc_md323", null ]
       ] ]
     ] ],
+    [ "07-Aug-2020 Product photos", "wip20200807.html", [
+      [ "Stability", "wip20200807.html#h20200807", null ],
+      [ "Canon 5D Mark III", "wip20200807.html#autotoc_md324", null ]
+    ] ],
     [ "Archives", "archive.html", "archive" ],
     [ "Photography", "photography.html", "photography" ],
     [ "About Me", "AboutMe.html", null ],
@@ -126,7 +79,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "AboutMe.html",
-"wip20190312.html#autotoc_md193"
+"wip20190312.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
